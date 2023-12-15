6dc2fd4191517e849ba8f80072769a7aa0435f60
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-12-15 19:47:27 -0400

    wip: add a little bit of animation in social buttons

---
+++ lib/components/social_button.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+import 'package:portfolio/style.dart';
+
+class SocialButton extends StatefulWidget {
+  const SocialButton({
+    super.key,
+    required this.content,
+    required this.icon,
+  });
+
+  static final borderRadius = BorderRadius.circular(6);
+
+  final String content;
+  final IconData icon;
+
+  @override
+  State<SocialButton> createState() => _SocialButtonState();
+}
+
+class _SocialButtonState extends State<SocialButton> {
+  static const padding = EdgeInsets.symmetric(horizontal: 18, vertical: 8);
+
+  var isMouseInside = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final content = [
+      FaIcon(widget.icon, color: Colors.white, size: 44),
+      const Gap(16),
+      Container(width: 1, height: 44, color: Colors.white),
+      const Gap(16),
+      Text(widget.content, style: Style.normalStyle),
+    ];
+
+    final button = Container(
+      padding: padding,
+      decoration: BoxDecoration(
+          color: Style.sideColor,
+          borderRadius: SocialButton.borderRadius,
+          border: Border.all(
+            color: isMouseInside ? Colors.blue[900]! : Colors.transparent,
+          )),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: content,
+      ),
+    );
+
+    return MouseRegion(
+      onEnter: (event) => setState(() {
+        isMouseInside = true;
+      }),
+      onExit: (event) => setState(() {
+        isMouseInside = false;
+      }),
+      cursor: SystemMouseCursors.click,
+      child: Material(
+        borderRadius: SocialButton.borderRadius,
+        color: isMouseInside ? Colors.white : Colors.transparent,
+        shadowColor: isMouseInside ? Style.sideColor : null,
+        elevation: 12,
+        child: button,
+      ),
+    );
+  }
+}
--- lib/views/home/section_buttons.dart
+++ lib/views/home/section_buttons.dart
@@ -1,41 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:gap/gap.dart';
 import 'package:icons_plus/icons_plus.dart';
-
-import 'package:portfolio/style.dart';
+import 'package:portfolio/components/social_button.dart';
 
 class ButtonsSection extends StatelessWidget {
   const ButtonsSection({super.key});
-
-  static final borderRadius = BorderRadius.circular(6);
-
-  Widget _buildButton({
-    required String text,
-    required IconData icon,
-  }) {
-    final button = Container(
-      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
-      decoration: BoxDecoration(
-        color: Style.sideColor,
-        borderRadius: borderRadius,
-      ),
-      child: Row(mainAxisSize: MainAxisSize.min, children: [
-        FaIcon(icon, color: Colors.white, size: 44),
-        const Gap(16),
-        Container(width: 1, height: 44, color: Colors.white),
-        const Gap(16),
-        Text(text, style: Style.normalStyle),
-      ]),
-    );
-
-    return Material(
-      borderRadius: borderRadius,
-      color: Colors.transparent,
-      elevation: 12,
-      child: button,
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,8 +18,8 @@ class ButtonsSection extends StatelessWidget {
         spacing: 35,
         runSpacing: 20,
         children: buttons.entries
-            .map((e) => _buildButton(
-                  text: e.key,
+            .map((e) => SocialButton(
+                  content: e.key,
                   icon: e.value,
                 ))
             .toList());
